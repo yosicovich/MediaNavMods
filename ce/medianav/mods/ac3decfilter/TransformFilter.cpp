b2b7ca83edb5de468b9d747a52d4e75ea2c8a744
developer <developer@medianav.ru>
2019-01-24 08:11:10 +0000

    +Change: Switch AC3DecoderFilter to liba52. Initial commit. it works more or less. Still has issues with sound level normalization.

--- ce/medianav/mods/ac3decfilter/TransformFilter.cpp
+++ ce/medianav/mods/ac3decfilter/TransformFilter.cpp
@@ -4,7 +4,7 @@
 
 #include "TransformFilter.h"
 #include <audshow.h>
-#include "cmnMemory.h"
+#include <libao/audio_out_internal.h>
 
 // --- registration tables ----------------
 
@@ -49,6 +49,11 @@ AC3DecoderFilter::m_sudFilter =
     AC3DecoderFilter::m_sudPins    // list of pins to register
 };
 
+// Configuration
+static const int cOutBufferTimeS = 1; // Buffer length in seconds
+static const int cSampleSizePerChannel = 2; //sizeof(short); 
+static const int cAC3OneChannelOutBlockSize = 256 * cSampleSizePerChannel;//256(samples per channel) * size of sample
+
 // ---- construction/destruction and COM support -------------
 
 // the class factory calls this to create the filter
@@ -56,38 +61,45 @@ AC3DecoderFilter::m_sudFilter =
 CUnknown* WINAPI 
 AC3DecoderFilter::CreateInstance(LPUNKNOWN pUnk, HRESULT* phr)
 {
-    return new AC3DecoderFilter(pUnk, phr);
+    return new AC3DecoderFilter(pUnk, phr, 3840);// buffer to hold one frame at maximal possible bitrate.
 }
 
-AC3DecoderFilter::AC3DecoderFilter(LPUNKNOWN pUnk, HRESULT* phr)
+AC3DecoderFilter::AC3DecoderFilter(LPUNKNOWN pUnk, HRESULT* phr, long frameBufferSize)
 :CTransformFilter(NAME("AC3DecoderFilter"), pUnk, *m_sudFilter.clsID)
-,m_hCodec(NULL)
+,m_a52State(NULL)
 ,m_maxChannels(0)
 ,m_maxOutputBufUsed(0)
+,m_a52OneOutBlockSize(0)
+,m_frameBufferSize(frameBufferSize)
+,m_frameBuffer(NULL)
+,m_frameBufferUsed(0)
+,m_frameBufferPos(0)
+,m_a52OutputSpeakersConfig(A52_CHANNEL)
 #if DBG > 0
+,m_dbgMaxFrameProcessTime(0)
 ,m_dbgAvgFrameProcessTime(0)
 #endif
 {
-    debugPrintf(DBG, L"AC3DecoderFilter::AC3DecoderFilter()\r\n");
-    yyGetAC3DecFunc(&m_audioAPI);
-    m_cMemOps.Alloc = cmnMemAlloc;
-    m_cMemOps.Copy = cmnMemCopy;
-    m_cMemOps.Free = cmnMemFree;
-    m_cMemOps.Set = cmnMemSet;
-    m_cMemOps.Check = cmnMemCheck;
-
-    VO_CODEC_INIT_USERDATA useData;
-    useData.memflag = VO_IMF_USERMEMOPERATOR;
-    useData.memData = (VO_PTR)(&m_cMemOps);	
-
-    debugPrintf(DBG, L"AC3DecoderFilter::AC3DecoderFilter() Init codec\r\n");
-    int returnCode = m_audioAPI.Init(&m_hCodec, VO_AUDIO_CodingAC3, &useData);
-    if(returnCode)
+    if(!m_frameBufferSize)
     {
-        debugPrintf(DBG, L"AC3DecoderFilter::AC3DecoderFilter() m_audioAPI.Init() failed: 0x%08X\r\n", returnCode);
+        debugPrintf(DBG, L"AC3DecoderFilter::AC3DecoderFilter() m_frameBufferSize == 0. Wrong configuration?\r\n");
         return;
     }
 
+    m_frameBuffer = new BYTE[m_frameBufferSize]; 
+    if(!m_frameBuffer)
+    {
+        debugPrintf(DBG, L"AC3DecoderFilter::AC3DecoderFilter() frame buffer allocation failed! Out of memory!\r\n");
+        return;
+    }
+
+    debugPrintf(DBG, L"AC3DecoderFilter::AC3DecoderFilter() Init codec\r\n");
+    m_a52State = a52_init();
+    if(!m_a52State)
+    {
+        debugPrintf(DBG, L"AC3DecoderFilter::AC3DecoderFilter() a52_init() failed\r\n");
+        return;
+    }
 }
 
 AC3DecoderFilter::~AC3DecoderFilter()
@@ -98,83 +110,197 @@ AC3DecoderFilter::~AC3DecoderFilter()
     {
         m_dbgAvgFrameProcessTime *= 1000000;
         m_dbgAvgFrameProcessTime /= freq.QuadPart;
+
+        m_dbgMaxFrameProcessTime *= 1000000;
+        m_dbgMaxFrameProcessTime /= freq.QuadPart;
     }else
     {
         debugPrintf(DBG, L"AC3DecoderFilter::~AC3DecoderFilter() QueryPerformanceFrequency() failed\r\n");
     }
-    debugPrintf(DBG, L"AC3DecoderFilter::~AC3DecoderFilter() m_maxOutputBufUsed=%d, avarage frame processing time = %I64d us\r\n", m_maxOutputBufUsed, m_dbgAvgFrameProcessTime);
+    debugPrintf(DBG, L"AC3DecoderFilter::~AC3DecoderFilter() m_maxOutputBufUsed=%d, avarage frame processing time = %I64d us, max frame processing time = %I64d us\r\n", m_maxOutputBufUsed, m_dbgAvgFrameProcessTime, m_dbgMaxFrameProcessTime);
 #else
     debugPrintf(DBG, L"AC3DecoderFilter::~AC3DecoderFilter() m_maxOutputBufUsed=%d\r\n", m_maxOutputBufUsed);
 #endif
-    if(m_hCodec)
+    if(m_a52State)
     {
-        m_audioAPI.Uninit(m_hCodec);
+        a52_free (m_a52State);
     }
+
+    if(m_frameBuffer)
+        delete[] m_frameBuffer;
+}
+
+static const int cAC3HeaderSize = 7;
+HRESULT AC3DecoderFilter::decodeOneFrame(TTransformBuffersState& buffersState)
+{
+    int frameDataSize = 0;
+    int flags, sampleRate, bitRate;
+
+    while(!frameDataSize && buffersState.inDataSize >= cAC3HeaderSize)
+    {
+        frameDataSize = a52_syncinfo(buffersState.inData, &flags, &sampleRate, &bitRate);
+        debugPrintf(DBG_TRACE, L"AC3DecoderFilter::decodeOneFrame() frameDataSize = %d\r\n", frameDataSize);
+        if(!frameDataSize)
+        {
+            ++buffersState.inData;
+            --buffersState.inDataSize;
+        }
+    }
+
+    if(!frameDataSize)
+        return VFW_E_BUFFER_UNDERFLOW;
+
+    if(buffersState.inDataSize < frameDataSize)
+    {
+        debugPrintf(DBG_TRACE, L"AC3DecoderFilter::decodeOneFrame() inDataSize = %d < frameDataSize = %d\r\n", buffersState.inDataSize, frameDataSize);
+        return VFW_E_BUFFER_UNDERFLOW;
+    }
+
+    // Here can be some checks against stream encoding changes from original one but who matters?
+
+    if(buffersState.outBufSize < m_oneFrameOutSize)
+    {
+        debugPrintf(DBG, L"AC3DecoderFilter::decodeOneFrame() outBufSize = %d < m_oneFrameOutSize = %d\r\n", buffersState.outBufSize, m_oneFrameOutSize);
+        return VFW_E_BUFFER_OVERFLOW;
+    }
+
+    // Override flags with chosen ones to down/up mix as requested.
+    flags = m_a52Flags;
+    level_t level = m_a52Level;
+    sample_t bias = 0; // No biasing is required for fixed point implementation.
+    if(a52_frame(m_a52State, buffersState.inData, &flags, &level, bias))
+    {
+        debugPrintf(DBG, L"AC3DecoderFilter::decodeOneFrame() a52_frame() failed!!!\r\n");
+        return E_FAIL;
+    }
+
+    for ( int i = 0; i < 6; i++) 
+    {
+        if (a52_block(m_a52State))
+        {
+            debugPrintf(DBG, L"AC3DecoderFilter::decodeOneFrame() a52_block() failed!!!\r\n");
+            return E_FAIL;
+        }
+        sample2s16_multi(a52_samples(m_a52State), (int16_t *)buffersState.outBuf, m_a52OutputSpeakersConfig);
+        buffersState.outBufSize -= m_a52OneOutBlockSize;
+        buffersState.outBuf += m_a52OneOutBlockSize;
+    }
+    buffersState.inDataSize -= frameDataSize;
+    buffersState.inData += frameDataSize;
+    return S_OK;
 }
 
 HRESULT AC3DecoderFilter::Transform(IMediaSample *pIn, IMediaSample *pOut)
 {
     debugPrintf(DBG_TRACE, L"AC3DecoderFilter::Transform()\r\n");
-    VO_CODECBUFFER inData;
-    VO_CODECBUFFER outData;
-    VO_AUDIO_OUTPUTINFO outFormat;
-    pIn->GetPointer(&inData.Buffer);
-    inData.Length = pIn->GetActualDataLength();
-
-    pOut->GetPointer(&outData.Buffer);
-    size_t bufRemains = pOut->GetSize();
-    outData.Length = 0;
-
-    /* decode one amr block */
-    int returnCode = m_audioAPI.SetInputData(m_hCodec,&inData);
-    if(returnCode)
+    TTransformBuffersState buffersState;
+    BYTE* inData;
+    long inDataSize;
+    pIn->GetPointer(&inData);
+    inDataSize = pIn->GetActualDataLength();
+    /*// Reset frame buffer in case of seeking.
+    if(pIn->IsDiscontinuity())
     {
-        debugPrintf(DBG, L"AC3DecoderFilter::Transform(): m_audioAPI.SetInputData() failed: 0x%08X\r\n", returnCode);
-        return E_FAIL;
-    }
-    
-    do 
+        debugPrintf(DBG_TRACE, L"AC3DecoderFilter::Transform() discontinuity detected! Reset frame buffer.\r\n");
+        m_frameBufferUsed = 0;
+    }*/
+
+    pOut->GetPointer(&buffersState.outBuf);
+    buffersState.outBufSize = pOut->GetSize();
+
+    while(inDataSize)
     {
-        outData.Buffer = reinterpret_cast<VO_PBYTE>(reinterpret_cast<size_t>(outData.Buffer) + outData.Length);
-        bufRemains -=outData.Length;
-        outData.Length = bufRemains;
-#if DBG > 0
-        LARGE_INTEGER startTime;
-        bool measureFailed = false;
-        if(!QueryPerformanceCounter(&startTime))
+        if(buffersState.outBufSize < m_oneFrameOutSize)
         {
-            debugPrintf(DBG, L"AC3DecoderFilter::Transform() QueryPerformanceCounter() failed at start\r\n");
-            measureFailed = true;
+            debugPrintf(DBG, L"AC3DecoderFilter::Transform() buffersState.outBufSize = %d < m_oneFrameOutSize = %d, possible causes - missed discontinuity or decode failure. Drop whole sample.\r\n", buffersState.outBufSize, m_oneFrameOutSize);
+            break;
         }
-#endif
-        returnCode = m_audioAPI.GetOutputData(m_hCodec, &outData, &outFormat);
-#if DBG > 0
-        if(!measureFailed)
+
+        if(m_frameBufferUsed < m_frameBufferSize)
         {
-            LARGE_INTEGER endTime;
-            if(!QueryPerformanceCounter(&endTime))
+            long fillSize = min(m_frameBufferSize - m_frameBufferUsed, inDataSize);
+            memcpy(&(m_frameBuffer[m_frameBufferUsed]), inData, fillSize);
+            inData += fillSize;
+            m_frameBufferUsed += fillSize;
+            inDataSize -= fillSize;
+        }
+
+        buffersState.inData = m_frameBuffer;
+        buffersState.inDataSize = m_frameBufferUsed;
+
+        HRESULT frameProcessResult;
+        do 
+        {
+#if DBG > 0
+            LARGE_INTEGER startTime;
+            bool measureFailed = false;
+            if(!QueryPerformanceCounter(&startTime))
             {
-                debugPrintf(DBG, L"AC3DecoderFilter::Transform() QueryPerformanceCounter() failed at and\r\n");
-            }else
+                debugPrintf(DBG, L"AC3DecoderFilter::Transform() QueryPerformanceCounter() failed at start\r\n");
+                measureFailed = true;
+            }
+#endif
+            frameProcessResult = decodeOneFrame(buffersState);
+#if DBG > 0
+            if(!measureFailed)
             {
-                LONGLONG processValue = endTime.QuadPart - startTime.QuadPart;
-                if(m_dbgAvgFrameProcessTime == 0)
-                    m_dbgAvgFrameProcessTime = processValue;
-                else
+                LARGE_INTEGER endTime;
+                if(!QueryPerformanceCounter(&endTime))
                 {
-                    m_dbgAvgFrameProcessTime = (m_dbgAvgFrameProcessTime + processValue) / 2;
+                    debugPrintf(DBG, L"AC3DecoderFilter::Transform() QueryPerformanceCounter() failed at and\r\n");
+                }else
+                {
+                    LONGLONG processValue = endTime.QuadPart - startTime.QuadPart;
+                    if(m_dbgAvgFrameProcessTime == 0)
+                        m_dbgAvgFrameProcessTime = processValue;
+                    else
+                    {
+                        m_dbgAvgFrameProcessTime = (m_dbgAvgFrameProcessTime + processValue) / 2;
+                    }
+
+                    if(m_dbgMaxFrameProcessTime < processValue)
+                        m_dbgMaxFrameProcessTime = processValue;
                 }
             }
-        }
 #endif
+        }while(frameProcessResult == S_OK && buffersState.inDataSize);
+        
+        if(frameProcessResult == E_FAIL)
+        {
+            debugPrintf(DBG, L"AC3DecoderFilter::Transform() decodeOneFrame() == E_FAIL. Possible causes - missed discontinuity or decode failure. Drop whole sample.\r\n");
+            // Reset frame buffer 
+            m_frameBufferUsed = 0;
+            break;
+        }
 
-    } while (!returnCode);
-    if(returnCode && returnCode != VO_ERR_INPUT_BUFFER_SMALL)
-    {
-        debugPrintf(DBG, L"AC3DecoderFilter::Transform(): m_audioAPI.GetOutputData() failed: 0x%08X\r\n", returnCode);
-        return E_FAIL;
+        if(buffersState.inDataSize)
+        {
+            if(buffersState.inDataSize == m_frameBufferUsed)
+            {
+                if(buffersState.outBufSize == pOut->GetSize())
+                {
+                    debugPrintf(DBG, L"AC3DecoderFilter::Transform() Bad frame data detected. Discarding it.\r\n");
+                    m_frameBufferUsed = 0;
+                    continue;
+                }
+            }else
+            {
+                memcpy(m_frameBuffer, &(m_frameBuffer[m_frameBufferUsed - buffersState.inDataSize]), buffersState.inDataSize);
+            }
+        }
+        m_frameBufferUsed = buffersState.inDataSize;
+
+        if(frameProcessResult == VFW_E_BUFFER_OVERFLOW)
+        {
+            debugPrintf(DBG, L"AC3DecoderFilter::Transform() decodeOneFrame() == VFW_E_BUFFER_OVERFLOW. No more output space!\r\n");
+            break;
+        }else if(frameProcessResult == VFW_E_BUFFER_UNDERFLOW)
+        {
+            // Normal situation. Let frame buffer be populated with the data from current sample if any still available.
+        }
     }
-    long usedBuffer = pOut->GetSize() - bufRemains;
+
+    long usedBuffer = pOut->GetSize() - buffersState.outBufSize;
     if(m_maxOutputBufUsed < usedBuffer)
         m_maxOutputBufUsed = usedBuffer;
     pOut->SetActualDataLength(usedBuffer);
@@ -185,7 +311,7 @@ HRESULT AC3DecoderFilter::Transform(IMediaSample *pIn, IMediaSample *pOut)
 
 HRESULT AC3DecoderFilter::CheckInputType(const CMediaType *mtIn)
 {
-    if(!m_hCodec)
+    if(!m_a52State)
     {
         debugPrintf(DBG, L"AC3DecoderFilter::CheckInputType(): codec is not initialized!\r\n");
         return E_UNEXPECTED;
@@ -201,6 +327,10 @@ HRESULT AC3DecoderFilter::CheckInputType(const CMediaType *mtIn)
     m_curOutputWfx.cbSize = 0;
     m_curOutputWfx.wFormatTag = WAVE_FORMAT_PCM;
     m_maxChannels = m_curOutputWfx.nChannels;
+    
+    // Nothing more than STEREO is supported
+    if(m_maxChannels > 2)
+        m_maxChannels = 2;
 
     m_mediaType.InitMediaType();
     m_mediaType.SetType(&MEDIATYPE_Audio);
@@ -221,7 +351,7 @@ HRESULT AC3DecoderFilter::CheckTransform(const CMediaType *mtIn, const CMediaTyp
 
     if(*mtOut->Type() != MEDIATYPE_Audio)
         return VFW_E_TYPE_NOT_ACCEPTED;
-    if(*mtOut->Subtype() != MEDIASUBTYPE_PCM)
+    if(*mtOut->Subtype() != MEDIASUBTYPE_PCM && *mtOut->Subtype() != MEDIASUBTYPE_PostProcPCM )
         return VFW_E_TYPE_NOT_ACCEPTED;
     if(*mtOut->FormatType() != FORMAT_WaveFormatEx)
         return VFW_E_TYPE_NOT_ACCEPTED;
@@ -231,18 +361,21 @@ HRESULT AC3DecoderFilter::CheckTransform(const CMediaType *mtIn, const CMediaTyp
     
     if(pInWfx->nSamplesPerSec != pOutWfx->nSamplesPerSec)
         return VFW_E_TYPE_NOT_ACCEPTED;
-    if(pInWfx->nChannels < pOutWfx->nChannels)
+    if(m_maxChannels < pOutWfx->nChannels)
         return VFW_E_TYPE_NOT_ACCEPTED;
     return S_OK;
 }
 
-static const int cBufferTimeS = 1; // Buffer length in seconds
 HRESULT AC3DecoderFilter::DecideBufferSize(IMemAllocator *pAlloc, ALLOCATOR_PROPERTIES *pProperties)
 {
     debugPrintf(DBG, L"AC3DecoderFilter::DecideBufferSize()\r\n");
     if(m_pInput->IsConnected() == FALSE)
         return E_UNEXPECTED;
-    size_t bufferSize = m_curOutputWfx.nAvgBytesPerSec * cBufferTimeS;
+    size_t bufferSize = m_curOutputWfx.nAvgBytesPerSec * cOutBufferTimeS;
+
+    // Make buffer frame aligned.
+    if( bufferSize % m_oneFrameOutSize > 0)
+        bufferSize += m_oneFrameOutSize - (bufferSize % m_oneFrameOutSize);
 
     ASSERT(pAlloc);
     ASSERT(pProperties);
@@ -281,8 +414,8 @@ HRESULT AC3DecoderFilter::GetMediaType(int iPosition, CMediaType *pMediaType)
     if(m_maxChannels <= iPosition / 2)
         return VFW_S_NO_MORE_ITEMS;
     m_curOutputWfx.nChannels = m_maxChannels - iPosition / 2;
-    m_curOutputWfx.wBitsPerSample = 8 * sizeof(short);
-    m_curOutputWfx.nBlockAlign = m_curOutputWfx.nChannels * sizeof(short);
+    m_curOutputWfx.wBitsPerSample = 8 * cSampleSizePerChannel;
+    m_curOutputWfx.nBlockAlign = m_curOutputWfx.nChannels * cSampleSizePerChannel;
     m_curOutputWfx.nAvgBytesPerSec = m_curOutputWfx.nBlockAlign * m_curOutputWfx.nSamplesPerSec;
     if(iPosition % 2 == 0)
         m_mediaType.SetSubtype(&MEDIASUBTYPE_PostProcPCM);
@@ -304,43 +437,27 @@ HRESULT AC3DecoderFilter::SetOutputMediaType(const CMediaType *pmt)
 
     m_curOutputWfx = *reinterpret_cast<PWAVEFORMATEX>(pmt->Format());
 
-    int paramValue = m_curOutputWfx.nChannels > 2 ? 1 : 0;
-    //Separate sub-woofer channel
-    if(m_audioAPI.SetParam(m_hCodec, VO_PID_AC3_OUTLFEON, &paramValue))
-        return E_FAIL;
+    m_a52Flags = A52_ADJUST_LEVEL;
 
-/*
-    0 = reserved
-    1 = 1/0 (C)
-    2 = 2/0 (L, R)
-    3 = 3/0 (L, C, R)
-    4 = 2/1 (L, R, l)
-    5 = 3/1 (L, C, R, l)
-    6 = 2/2 (L, R, l, r)
-    7 = 3/2 (L, C, R, l, r)
-*/
     switch(m_curOutputWfx.nChannels)
     {
-    case 0:
-        return E_FAIL;
     case 1:
-    case 2:
-        paramValue = m_curOutputWfx.nChannels;
+        m_a52OutputSpeakersConfig = A52_MONO;
         break;
-    case 3:
-    case 4:
-        paramValue = 6;
+    case 2:
+        m_a52OutputSpeakersConfig = A52_STEREO;
         break;
     default:
-        paramValue = 7;
+        return E_FAIL;
         break;
     }
-    if(m_audioAPI.SetParam(m_hCodec, VO_PID_AC3_OUTPUTMODE, &paramValue))
-        return E_FAIL;
+    m_a52Flags |= m_a52OutputSpeakersConfig;
 
-    paramValue = m_curOutputWfx.nChannels;
-    if(m_audioAPI.SetParam(m_hCodec, VO_PID_AC3_NUMCHANS, &paramValue))
-        return E_FAIL;
+    // Gain level 1 means no gain.
+    m_a52Level = a52_to_level(1);
+
+    m_a52OneOutBlockSize = cAC3OneChannelOutBlockSize * m_curOutputWfx.nChannels;
+    m_oneFrameOutSize = m_a52OneOutBlockSize * 6; // AC3 has 6 blocks per frame.
 
     debugPrintf(DBG, L"AC3DecoderFilter::SetOutputMediaType() nChannels=%d, SubType=%s\r\n", static_cast<unsigned int>(m_curOutputWfx.nChannels), *pmt->Subtype() == MEDIASUBTYPE_PostProcPCM ? L"MEDIASUBTYPE_PostProcPCM" : L"MEDIASUBTYPE_PCM");
     return S_OK;
--- ce/medianav/mods/ac3decfilter/TransformFilter.h
+++ ce/medianav/mods/ac3decfilter/TransformFilter.h
@@ -5,7 +5,7 @@
 
 
 #include "StdAfx.h"
-#include "voAC3.h"
+#include "a52.h"
 
 class DECLSPEC_UUID("1F3F5741-A9EE-4bd9-B64E-99C5534B3817") // ac3decfilter
 AC3DecoderFilter: public CTransformFilter
@@ -37,26 +37,46 @@ public:
 
         return S_OK;
     }
+protected:
+    typedef struct  
+    {
+        BYTE* inData;
+        long  inDataSize;
+        BYTE* outBuf;
+        long  outBufSize;
+    }TTransformBuffersState;
+
+    HRESULT SetOutputMediaType(const CMediaType *pmt);
+    HRESULT decodeOneFrame(TTransformBuffersState& buffersState);
 private:
     // construct only via class factory
-    AC3DecoderFilter(LPUNKNOWN pUnk, HRESULT* phr);
-    HRESULT SetOutputMediaType(const CMediaType *pmt);
+    AC3DecoderFilter(LPUNKNOWN pUnk, HRESULT* phr, long frameBufferSize);
 private:
     // Codec specific
-    VO_MEM_OPERATOR m_cMemOps;
-    VO_AUDIO_CODECAPI m_audioAPI;
-    VO_HANDLE m_hCodec;
+    a52_state_t *m_a52State;
+    int m_a52Flags;
+    level_t m_a52Level;
+    int m_a52OutputSpeakersConfig;
+    long m_a52OneOutBlockSize;
+    
+    // Frame buffering
+    long m_frameBufferSize;
+    BYTE* m_frameBuffer;
+    long m_frameBufferUsed;
+    long m_frameBufferPos;
 
     // Output data
     int m_maxChannels;
     WAVEFORMATEX m_curOutputWfx;
     CMediaType m_mediaType;
+    long m_oneFrameOutSize;
 
     // Misc
     long m_maxOutputBufUsed;
 
 #if DBG > 0
     //Debug
+    LONGLONG m_dbgMaxFrameProcessTime;
     LONGLONG m_dbgAvgFrameProcessTime;
 #endif
 
